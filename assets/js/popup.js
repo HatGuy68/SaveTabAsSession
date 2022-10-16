1ca13f5a85d588d1ef4070638cbeb30f7aa67314
HatGuy68 <irahul.2k@gmail.com>
2022-10-16 12:35:45 +0530

    add session feature added

--- assets/js/popup.js
+++ assets/js/popup.js
@@ -1,11 +1,28 @@
+document.getElementById("addSession").addEventListener('click', addSession);
+
+function addSession() {
+    sessionName = prompt('Add current tabs to session')
+    saveSession(sessionName)
+    console.log('session save started');
+}
+
 async function getAllTab() {
     let queryOptions = {populate:true};
     // `tab` will either be a `tabs.Tab` instance or `undefined`.
     let tabs = await chrome.windows.getLastFocused(queryOptions)
+    console.log(tabs);
     return tabs;
 }
 
-function rebootSession(sessionName)
+async function rebootSession(sessionName) {
+    sessionUrls = []
+    await chrome.storage.local.get([sessionName]).then(list => {
+        list[sessionName].forEach(object => {
+            sessionUrls.push(object.url)
+        });
+    })
+    chrome.windows.create({url: sessionUrls})
+}
 
 async function saveSession(sessionName) {
     let sessionList = []
@@ -14,8 +31,8 @@ async function saveSession(sessionName) {
             sessionList.push({"title": tab.title, "url": tab.url})
         });
     })
-    await chrome.storage.local.set({sessionName: sessionList}, function() {
-        console.log('Settings saved', {sessionName: sessionList});
+    await chrome.storage.local.set({[sessionName]: sessionList}, function() {
+        console.log('Session saved', {[sessionName]: sessionList});
     });
 }
 
@@ -33,9 +50,12 @@ function renderSession(sessionName) {
     let session_list = document.getElementById('session_list')
     console.log(session_list);
     let li = document.createElement("li")
-    li.innerHTML = `<li class="session"><div class="session_title" onclick="rebootSession(${sessionName})">${sessionName}</div><div class="session_delete" onclick="deleteSession("${sessionName}")">X</div></li>`
+    li.innerHTML = `<li class="session"><div class="session_title" id="title-${sessionName}">${sessionName}</div><div class="session_delete" id="delete-${sessionName}">X</div></li>`
     session_list.appendChild(li)
+    document.getElementById(`title-${sessionName}`).addEventListener('click', () => {
+        rebootSession(`${sessionName}`)
+    })
+    // document.getElementById(`delete-${sessionName}`).addEventListener('click', removeSession(`${sessionName}`))
 }
 
-saveSession('test')
-console.log(listSessions())
+listSessions()
