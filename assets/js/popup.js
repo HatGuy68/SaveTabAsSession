a59f27fc60759c81d42413738c70a8ad027b119e
Shaziah Gafur <shaziah_g7@hotmail.com>
2022-12-06 10:20:07 -0500

    Improved design for sidebar in chrome extension mode

--- assets/js/popup.js
+++ assets/js/popup.js
@@ -1,17 +1,25 @@
 let messageDiv = document.getElementById('message') 
 
 document.addEventListener('DOMContentLoaded', function() {
-    var link = document.getElementById('open');
+    var link = document.getElementById('open-nav');
     // onClick's logic below:
     link.addEventListener('click', function() {
         openNav();
     });
 });
 
+document.addEventListener('DOMContentLoaded', function() {
+    var link = document.getElementById('close-nav');
+    // onClick's logic below:
+    link.addEventListener('click', function() {
+        closeNav();
+    });
+});
+
 /* Set the width of the sidebar to 250px and the left margin of the page content to 250px */
 function openNav() {
-    document.getElementById("mySidebar").style.width = "250px";
-    document.getElementById("main").style.marginLeft = "250px";
+    document.getElementById("mySidebar").style.width = "150px";
+    document.getElementById("main").style.marginLeft = "150px";
 }
 
 /* Set the width of the sidebar to 0 and the left margin of the page content to 0 */
