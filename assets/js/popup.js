8df73e26972f042489580066a4f6a71968d21258
Shaziah Gafur <shaziah_g7@hotmail.com>
2022-12-06 07:27:55 -0500

    Added footer to sidebar

--- assets/js/popup.js
+++ assets/js/popup.js
@@ -12,7 +12,6 @@ document.getElementById("mySidebar").style.width = "0";
 document.getElementById("main").style.marginLeft = "0";
 }
 
-
 function addSession() {
     sessionName = prompt('Add current tabs to session')
     saveSession(sessionName)
